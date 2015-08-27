3d24123bf3a7bc50921b0d0fa44b7cecfea211f7
Nick Prokesch <nick@prokes.ch>
2015-08-26 21:22:44 -0500

    Remove location info from namechange feed

--- app/assets/javascripts/components/NameChangeFeed.js.jsx
+++ app/assets/javascripts/components/NameChangeFeed.js.jsx
@@ -42,24 +42,20 @@ var NameChangeFeed = React.createClass({
         }
         var fullString = item.old_name + newNames;
         if (item.match) {
-          var gender = item['match']['gender'].;
-          var location = item['match']['state'];
-          var age = item['match']['age'];
+          var gender = item.match.gender;
+          var location = item.match.state;
+          var age = item.match.age;
         }
         return (
             <div className='row' key={i}>
               <div className='columns small-12 medium-12 large-12'>
-                <p className={classes} id='react-div'>{fullString}
-                  {if (item.match) {
-                      return (<span>{age}/{gender}/{location}</span>)
-                  }}
-                </p>
+                <p className={classes} id='react-div'>{fullString}</p>
               </div>
             </div>
         );
       });
     } else {
-      var feedItems = <div className='loading'>Loading...</div>;
+      var feedItems = (<div className='loading'>Loading...</div>);
     };
 
     return (
