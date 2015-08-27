a79d13895c3c31ee1032660c81295f463e6b8b8b
Nick Prokesch <nick@prokes.ch>
2015-08-26 22:39:34 -0500

    Add crawl session create

--- app/models/crawl_session.rb
+++ app/models/crawl_session.rb
@@ -1,7 +1,23 @@
 # Handles creating and recalling okcupid sessions
 class CrawlSession
+  def self.create(opts)
+    browser = Browser.new(username: opts[:username], password: opts[:password])
+    browser.login
+    self.cookies = browser.save_session if browser.is_logged_in?
+    browser.is_logged_in?
+  end
+
+  def self.get
+    browser = Browser.new(session: cookies, user_agent: user_agent)
+    browser.is_logged_in?
+  end
+
   def self.cookies
     $redis.get('cookies')
+  end
+
+  def self.cookies=(cookies)
+    $redis.set('cookies', cookies)
   end
 
   def self.user_agent
